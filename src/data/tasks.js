2dcb4cb43851ed93cba8676f3cf5744b99bf21a0
Julian Murcia <julianazange@gmail.com>
2023-07-14 23:51:56 -0500

    init

---
+++ src/data/tasks.js
@@ -0,0 +1,22 @@
+export const tasks = [
+    {
+        id: 0,
+        title: 'Estudiar React',
+        description: 'Estudiar React y hacer una app',
+    },
+    {
+        id: 1,
+        title: 'Estudiar Vite',
+        description: 'Estudiar Vite y hacer una app',
+    },
+    {
+        id: 2,
+        title: 'Estudiar Angular',
+        description: 'Estudiar Angular y hacer una app',
+    },
+    {
+        id: 3,
+        title: 'Estudiar Vue',
+        description: 'Estudiar Vue y hacer una app',
+    },
+]
